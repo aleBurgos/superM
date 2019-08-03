5343d52abc119d4e9be5d4a72251ba14b134fbe6
manuel burgos <manuelalejandro303@gmail.com>
2019-08-02 21:47:09 -0300

    integration of react-navigation

--- App.js
+++ App.js
@@ -9,17 +9,23 @@
 import React, {Fragment} from 'react';
 import {View, Text} from 'react-native';
 import styles from './App.less';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 
 const App = () => {
   return (
     <Fragment >
-      <View style={styles.app}>
+      <View style={[styles.app,{backgroundColor:styles.grey}]}>
         <Text>working on!</Text>
       </View>
     </Fragment>
   );
 };
 
+const TabNavigator = createBottomTabNavigator({
+  Home: App,
+});
 
-export default App;
+export default createAppContainer(TabNavigator);
+
+// export default App;
